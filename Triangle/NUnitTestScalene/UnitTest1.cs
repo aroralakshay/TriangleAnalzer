e53678955bd3081794a17fc6d78f4b42be5cda50
Lakshay Arora <Larora0952@conestogac.on.ca>
2020-10-07 18:23:37 -0400

    My first Commit

---
+++ Triangle/NUnitTestScalene/UnitTest1.cs
@@ -0,0 +1,15 @@
+using NUnit.Framework;
+using Triangle;
+
+namespace NUnitTestScalene
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        
+    }
+}
---
+++ Triangle/Triangle/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool value = false;
+            while (!value)
+            {   
+                
+                Console.WriteLine("1. Enter Triangle dimensons");
+                Console.WriteLine("2. Exit");
+                String s = Console.ReadLine();
+                if (s == "2")
+                {
+                    System.Environment.Exit(1);
+                    value = true;
+                }
+                else if (s == "1")
+                {
+                    int x = Convert.ToInt32(Console.ReadLine());
+                    int y = Convert.ToInt32(Console.ReadLine());
+                    int z = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine(TriangleSolver.Analyze(x, y, z));
+                    value = false;
+                }
+
+                else
+                {
+                    Console.WriteLine("Invalid option choose between these two options only");
+                    value = false;
+                }
+            }
+           
+        }
+    }
+}
